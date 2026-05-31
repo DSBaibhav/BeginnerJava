5f93a4ad6fcffaa1eab9872ff3d0378ec1595dfc
DSBaibhav <baibhavsingh473@gmail.com>
2026-05-31 22:25:20 +0530

    Function to check for Krishnamurthy number

---
+++ functions/Strong.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Strong{
+    public static boolean isStrong(int num){
+        int sum=0;
+        int temp=num;
+        while(temp>0){
+            int digit=temp%10;
+            int fact=1;
+            for(int i=1;i<=digit;i++){
+                fact*=i;
+            }
+            sum+=fact;
+            temp/=10;
+        }
+        return sum==num;
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n=sc.nextInt();
+        if(isStrong(n)){
+            System.out.println(n+" is a strong number.");
+        }
+        else{
+            System.out.println(n+" is not  a strong number.");
+        }
+    }
+}
