ea536352bd4aa88046ef97797807304a3f5af878
DSBaibhav <baibhavsingh473@gmail.com>
2026-06-12 23:03:32 +0530

    Fibonacci with the help of recursion

---
+++ functions/recursion/FiboRecur.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class FiboRecur{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number of terms : ");
+        int num=sc.nextInt();
+        System.out.println("Fibonacci series up to "+num+" terms is : ");
+        for (int i = 0; i < num; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+        sc.close();
+    }
+    public static int fibonacci(int n){
+        if(n<=1){
+            return n;
+        }
+        return fibonacci(n-1)+fibonacci(n-2);
+    }
+}
