36ac40ad38ce96c1c4b862a7c1a099bc7e6a5e8f
DSBaibhav <baibhavsingh473@gmail.com>
2026-07-25 23:02:19 +0530

    Using OOPS concept to deal with maximum of three numbers

---
+++ oops/MaxOfThree.java
@@ -0,0 +1,30 @@
+package oops;
+import java.util.Scanner;
+public class MaxOfThree {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1=sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2=sc.nextInt();
+        System.out.print("Enter third number: ");
+        int num3=sc.nextInt();
+        MaxOfThree ob=new MaxOfThree();
+        int max=ob.max(num1,num2,num3);
+        System.out.println("Maximum of three numbers is: "+max);
+        sc.close();
+    }
+    public int max(int a,int b,int c){
+        int max;
+        if(a>=b && a>=c){
+            max=a;
+        }
+        else if(b>=a && b>=c){
+            max=b;
+        }
+        else{
+            max=c;
+        }
+        return max;
+    }
+}
