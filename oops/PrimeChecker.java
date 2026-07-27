cc1ec45453d11f6f05b9389cddd8cd65180e9c04
DSBaibhav <baibhavsingh473@gmail.com>
2026-07-27 23:55:05 +0530

    OOPS program to check prime number

---
+++ oops/PrimeChecker.java
@@ -0,0 +1,35 @@
+package oops;
+import java.util.Scanner;
+
+class PrimeChecker {
+
+    // Method to check if a number is prime
+    public boolean isPrime(int num) {
+
+        if (num <= 1)
+            return false;
+
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        PrimeChecker pc = new PrimeChecker();
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        if (pc.isPrime(n))
+            System.out.println(n + " is a Prime Number.");
+        else
+            System.out.println(n + " is Not a Prime Number.");
+
+        sc.close();
+    }
+}
