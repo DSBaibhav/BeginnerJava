38a6cb6aaaf9c2ae27bab1e597ffb47ccaf89cb3
DSBaibhav <baibhavsingh473@gmail.com>
2026-08-27 20:52:13 +0530

    Add abstract method stop() to AbstractionDemo1.java

--- oops/AbstractionDemo1.java
+++ oops/AbstractionDemo1.java
@@ -1,11 +1,16 @@
 package oops;
 abstract class Vehicle{
     abstract void start();
+    abstract void stop();
 }
 class Car extends Vehicle{
     @Override
     void start() {
         System.out.println("Car is starting with a key.");
+    }
+    @Override
+    void stop() {
+        System.out.println("Car is stopping with brakes.");
     }
 }
 class Bike extends Vehicle{
@@ -13,12 +18,18 @@ class Bike extends Vehicle{
     void start() {
         System.out.println("Bike is starting with a button.");
     }
+    @Override
+    void stop() {
+        System.out.println("Bike is stopping with brakes.");
+    }
 }
 public class AbstractionDemo1 {
     public static void main(String[] args) {
         Car car = new Car();
         car.start();
+        car.stop();
         Bike bike = new Bike();
         bike.start();
+        bike.stop();
     }
 }
